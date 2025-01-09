7ea93a02e854ac5a3836af05b648aff736b59fd3
amhlongo <amhlongo@dvtsoftware.com>
2025-01-09 14:03:27 +0200

    Added basic functionality

---
+++ calculator/main.js
@@ -0,0 +1,16 @@
+const values = []
+const operations = ['+', '-', '*', '/']
+
+function appendValue(value) {
+    console.log();
+    
+    if (operations.includes(value) && operations.includes(values.at(-1))) {
+        window.alert("Please press a number")
+        return
+    }
+    
+    values.push(value);
+
+    console.log(values.join(""));
+    
+}
