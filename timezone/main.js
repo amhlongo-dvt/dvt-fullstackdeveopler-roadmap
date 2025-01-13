db37d217db37dfec96ca3906faa2d1502c84bf34
Andile Mhlongo <majiyaz2@gmail.com>
2025-01-13 20:27:40 +0200

    Added timezone

--- timezone/main.js
+++ timezone/main.js
@@ -1,2 +1,14 @@
-console.log("Hello from JavaScript!");  
-console.log(moment().startOf('day').fromNow());
+// const dayjs = require("dayjs");
+dayjs.extend(window.dayjs_plugin_utc)
+dayjs.extend(window.dayjs_plugin_timezone)
+
+console.log("Hello from JavaScript!"); 
+
+const time = document.querySelector(".time")
+const date = document.querySelector(".date")
+const timezone = document.querySelector(".timezone")
+
+timezone.textContent = dayjs.tz.guess()
+date.textContent = dayjs().format('dddd D MMMM, YYYY')
+setInterval(function () {time.textContent = dayjs().format('HH:mm:ss');}, 1000);
+
