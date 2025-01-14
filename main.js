4083aa416221033d9745ea00d4b9dc46f1bbff05
amhlongo <amhlongo@dvtsoftware.com>
2025-01-14 16:12:28 +0200

    added and iframe

--- main.js
+++ main.js
@@ -1,40 +1,6 @@
 function loadHTML(url, elementId) {
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-          
-            
-            return response.text();
-        })
-        .then(html => {
-
-            const parser = new DOMParser()
-            const doc = parser.parseFromString(html, "text/html")
-            const winBody = doc.getElementsByTagName("body")[0].innerHTML
-            const styleUrl = "./"+url.split("/")[1]+"/style.css"
-            const scriptUrl = "./"+url.split("/")[1]+"/main.js"
-            const container = document.getElementById(elementId);
-            const newScript = document.createElement('script')
-            newScript.setAttribute('src', scriptUrl)
-            document.body.appendChild(newScript);
-
-
-            container.innerHTML = winBody;
-            
-            fetch(styleUrl).then(response => {
-               return response.text();
-            }).then(style => {
-                const newStyle =  document.createElement('style')
-                newStyle.innerHTML = style
-                container.prepend(newStyle)
-            })
-
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+    const container = document.getElementById(elementId);
+    container.setAttribute('src', url)
 }
 
 loadHTML("./calculator/index.html", "window-content")
