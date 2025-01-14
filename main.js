d9c33b5ecda7e202464a371f0a5c2f84b0cd6054
amhlongo <amhlongo@dvtsoftware.com>
2025-01-14 15:48:30 +0200

    Added js to the calculator

--- main.js
+++ main.js
@@ -13,47 +13,24 @@ function loadHTML(url, elementId) {
             const parser = new DOMParser()
             const doc = parser.parseFromString(html, "text/html")
             const winBody = doc.getElementsByTagName("body")[0].innerHTML
-            const winHead = doc.getElementsByTagName("head")[0]
-            const links = winHead.getElementsByTagName("link");
-            const head = document.getElementsByTagName("head")[0]
-            const newStyle =  document.createElement('link')
-            newStyle.setAttribute("rel", "stylesheet")
-            const newScript =  document.createElement('script')
-            // new
-            // newStyle.setAttribute("rel", "stylesheet")
-            // newStyle.setAttribute("href", "./"+url.split("/")[1]+"/style.css")
-
-            styleUrl = "./"+url.split("/")[1]+"/style.css"
-            scriptUrl = "./"+url.split("/")[1]+"/main.js"
-
-            console.log(newStyle);
-            document.head.appendChild(newStyle)
-            
-            
-            
+            const styleUrl = "./"+url.split("/")[1]+"/style.css"
+            const scriptUrl = "./"+url.split("/")[1]+"/main.js"
             const container = document.getElementById(elementId);
+            const newScript = document.createElement('script')
+            newScript.setAttribute('src', scriptUrl)
+            document.body.appendChild(newScript);
+
+
             container.innerHTML = winBody;
             
             fetch(styleUrl).then(response => {
                return response.text();
-
-               
             }).then(style => {
-                // console.log(style);
-                
-                const newStyle2 =  document.createElement('style')
-                newStyle2.innerHTML = style
-                container.prepend(newStyle2)
+                const newStyle =  document.createElement('style')
+                newStyle.innerHTML = style
+                container.prepend(newStyle)
             })
 
-
-            // // Execute scripts in the loaded HTML
-            // const scripts = doc.getElementsByTagName('script');
-            // for (let script of scripts) {
-            //     const newScript = document.createElement('script');
-            //     newScript.text = script.innerHTML;
-            //     document.body.appendChild(newScript);
-            // }
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
